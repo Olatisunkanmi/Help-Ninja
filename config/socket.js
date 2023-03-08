6034a838b007e00c4952f608f578166a3e1d7446
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2023-03-08 15:21:19 +0100

    refactor socket config

--- config/socket.js
+++ config/socket.js
@@ -2,58 +2,174 @@ const socket = require('socket.io');
 const Helper = require('./helper');
 const cors = require('cors');
 
+/**
+ * @class Socket
+ * @classdesc Socket class
+ * @param {object} server
+ * @returns {object} io
+ * @constructor Socket
+ */
 class Socket {
 	constructor(server) {
 		this.io = socket(server, { cors: { origin: '*' } });
 		this.botName = 'Help Ninja Bot';
-		this.initializeSocket();
+		this.userName = 'Olasunkanmi';
+		this.io = this.initializeSocket();
 	}
+
+	/**
+	 * @private
+	 * @function _emitUserMessage
+	 * @param {object} socket
+	 * @param {object} message
+	 * @emits userMessage - when a user sends a message
+	 * @memberof Socket
+	 * @returns {object} message - returns user message to the chatroom
+	 */
+	_emitUserMessage(socket, message) {
+		socket.emit('userMessage', Helper(this.userName, message));
+	}
+
+	/**
+	 * @private
+	 * @function _emitBotMessage
+	 * @param {object} socket
+	 * @param {object} message
+	 * @emits botMessage - when the chatbot sends a message
+	 * @memberof Socket
+	 * @returns {object} message - returns bot message to the chatroom
+	 */
+	_emitBotMessage(socket, message) {
+		socket.emit('botMessage', Helper(this.botName, message));
+	}
+
+	/**
+	 * @private
+	 * @function _emitNotification
+	 * @param {object} socket
+	 * @param {object} message
+	 * @emits notification - when a user joins or leaves the chatroom
+	 * @memberof Socket
+	 * @returns {object} message - returns notification to the chatroom
+	 */
+	_emitNotification(socket, message) {
+		socket.emit('notification', Helper(this.botName, message));
+	}
+
+	/**
+	 * @private
+	 * @function _broadcastNotification
+	 * @param {object} socket
+	 * @param {object} message
+	 * @emits notification - when a user joins or leaves the chatroom
+	 * @memberof Socket
+	 * @returns {function} _emitNotification - returns notification to the chatroom
+	 */
+	_broadcastNotification(socket, message) {
+		this._emitNotification(socket.broadcast, message);
+	}
+
+	/**
+	 * @private
+	 * @function _emitDisconnect
+	 * @param {object} socket
+	 * @param {object} message
+	 * @emits disconnect - when a user disconnects from the chatroom
+	 * @memberof Socket
+	 * @returns {function} _broadcastNotification - returns notification to the chatroom
+	 */
+	_emitDisconnect(socket, message) {
+		socket.on('disconnect', () => {
+			this._broadcastNotification(socket, message);
+		});
+	}
+
+	/**
+	 * @private
+	 * @function _listenToChatMessage
+	 * @param {object} socket
+	 * @listens for chatMessage - when a user sends a message
+	 * @emits userMessage
+	 * @memberof Socket
+	 * @returns {function} _emitUserMessage - returns user message to the chatroom
+	 *
+	 */
+	_listenToChatMessage(socket) {
+		socket.on('chatMessage', (msg) => {
+			this._emitUserMessage(socket, msg);
+		});
+	}
+
+	/**
+	 * @private
+	 * @function _listenRegister
+	 * @param {object} socket
+	 * @listens for register - when a user registers
+	 * @emits access - when a user registers and is granted access
+	 * @memberof Socket
+	 */
+	_listenRegister(socket) {
+		socket.on('register', (data) => {
+			this.io.emit('access', data);
+		});
+	}
+
+	//create a function to emit to a specific user
+	// _emitToUser(event, data, userId) {
+	// 	this.io.to(userId).emit(event, data);
+	// }
+
+	/**
+	 * @static
+	 * @function createSocket
+	 * @param {object} server
+	 * @memberof Socket
+	 * @returns {object} io - returns socket.io instance
+	 *
+	 */
 
 	initializeSocket() {
 		// Run when client connects
 		this.io.on('connection', (socket) => {
 			// Listen to new user connection
-			console.log(`New User Connected ${socket.id}`);
+			logger.info(`New User Connected ${socket.id}`);
 
-			// Welcome user
-			// Only user can see
-			socket.emit(
-				'botMessage',
-				Helper(
-					this.botName,
-					'Welcome to Help Ninja, I will be your personal AI assistant',
-				),
+			// Emit bot message
+			this._emitBotMessage(socket, 'Welcome to Help Ninja');
+
+			// Broadcast to chatroom
+			this._broadcastNotification(
+				socket,
+				'A User has joined the chat',
 			);
 
-			// Broadcast to channel except user
-			// Broadcast when a user connects
-			socket.broadcast.emit(
-				'notification',
-				Helper(this.botName, 'A User has joined the chat'),
-			);
+			// Emit to the new user only
+			this._emitNotification(socket, 'You have joined the chat');
 
-			// Everyone
-			// Runs when a client disconnects
-			socket.on('disconnect', () => {
-				this.io.emit(
-					'notification',
-					Helper(this.botName, 'A User has left the chat'),
-				);
-			});
+			//Emit when user disconnects
+			this._emitDisconnect(socket, 'A User has left the chat');
 
-			socket.on('chatMessage', (msg) => {
-				this.io.emit('userMessage', Helper('USER', msg));
-			});
+			// Listen for chatMessage
+			this._listenToChatMessage(socket);
 
-			socket.on('botMessage', (msg) => {
-				this.io.emit('message', Helper('USER', msg));
-			});
-
-			socket.on('register', (data) => {
-				this.io.emit('access', data);
-			});
+			// Listen for register
+			this._listenRegister(socket);
 		});
 	}
+
+	/**
+	 * @static
+	 * @function createSocket
+	 * @param {object} server
+	 * @memberof Socket
+	 * @returns {object} io - returns socket.io instance
+	 * @description Creates a socket instance
+	 */
+	// static createSocket(server) {
+	// 	const socketInstance = new Socket(server);
+
+	// 	return socketInstance.initializeSocket();
+	// }
 }
 
 module.exports = Socket;
