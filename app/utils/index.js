f97db0462b6d1b68486b9b14695891ba3f34929f
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2023-03-10 23:14:31 +0100

    Update index.js

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,12 +1,9 @@
 const genericError = require('./error/generic.error');
 const ApiError = require('./error/api.error');
 const constants = require('./constants');
-const { Helper, serverStatic } = require('./helper');
 
 module.exports = {
 	genericError,
 	ApiError,
-	Helper,
 	constants,
-	serverStatic,
 };
