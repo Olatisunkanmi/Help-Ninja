dec2ffe32b560de9727af9f210ef35c7b11706fb
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2023-03-13 15:34:43 +0100

    JUNKED

--- app/utils/index.js
+++
@@ -1,9 +0,0 @@
-const genericError = require('./error/generic.error');
-const ApiError = require('./error/api.error');
-const constants = require('./constants');
-
-module.exports = {
-	genericError,
-	ApiError,
-	constants,
-};
