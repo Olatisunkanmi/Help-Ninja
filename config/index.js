3834c39fb0ac9a583b99e8681b1c08dc38b3d7f1
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2023-03-13 15:57:10 +0100

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,6 +1,5 @@
-// const appConfig = require('./..xl');
 const appConfig = require('./app');
-const config = require('./env');
+// const config = require('./env');
 const Logger = require('./logger');
 
-module.exports = { appConfig, Logger, config };
+module.exports = { appConfig, Logger };
